bfb6b83b6f1aefc5a909adfaee3c6eae6320dd7e
Dinesh <dineshveguru123@gmail.com>
2022-12-05 20:23:54 +0530

    changes-dinesh

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { AuthContextProvider } from './Context/AuthContext';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Profile from './pages/Profile';
-import './styles/App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AuthContextProvider } from "./Context/AuthContext";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import "./styles/App.css";
 
 function App() {
-
   return (
     <>
-    <AuthContextProvider>
-      <BrowserRouter>
-      <div className="App">
-        <div className='freespace' ></div>
-        <div className="blur1" ></div>
-        <div className="blur2" ></div>
-        
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route path='/profile' element={<Profile/>} />
-            <Route path='/login' element={<Login/>} />
-          </Routes>
-      </div>
-      </BrowserRouter>
-    </AuthContextProvider>
-    
+      <AuthContextProvider>
+        <BrowserRouter>
+          <div className="App">
+            <div className="freespace"></div>
+            <div className="blur1"></div>
+            <div className="blur2"></div>
+
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </AuthContextProvider>
     </>
   );
 }
